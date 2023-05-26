6cd30b6215b2667124126e3ee26afe998dae2fd1
juva <juvet.quentin19@gmail.com>
2023-05-26 11:34:45 +0200

    fix create fun

--- index.js
+++ index.js
@@ -10,47 +10,69 @@ admin.initializeApp({
 
 
 const db = admin.firestore().collection("no-SQL");
+const objet = {};
+
 
 
 function dataImport(cheminFichierCSV) {
-    const fichierStream = fs.createReadStream(cheminFichierCSV);
-    const rl = readline.createInterface({
-      input: fichierStream,
-      crlfDelay: Infinity
-    });
-  
-    let isFirstLine = true;
-    let champs;
-    const promises = [];
-  
-    rl.on('line', (ligne) => {
-      if (isFirstLine) {
-        champs = ligne.split(',');
-        isFirstLine = false;
-      } else {
-        const valeurs = ligne.split(',');
-  
-        const document = {};
-        champs.forEach((champ, index) => {
-          document[champ] = valeurs[index];
-        });
-  
-        const promise = db.add(document);
-        promises.push(promise);
-      }
-    });
-  
-    rl.on('close', () => {
-      Promise.all(promises)
-        .then(() => {
-          console.log('Importation terminée.');
-          menu();
-        })
-        .catch((erreur) => {
-          console.error('Erreur lors de l\'importation des documents :', erreur);
-        });
-    });
-  }  
+  const fichierStream = fs.createReadStream(cheminFichierCSV);
+  const rl = readline.createInterface({
+    input: fichierStream,
+    crlfDelay: Infinity
+  });
+
+  let isFirstLine = true;
+  let champs;
+  const promises = [];
+  let totalLignes = 0;
+  let lignesImportees = 0;
+
+  rl.on('line', (ligne) => {
+    if (isFirstLine) {
+      champs = ligne.split(',');
+      isFirstLine = false;
+    } else {
+      const valeurs = ligne.split(',');
+
+      const document = {};
+      champs.forEach((champ, index) => {
+        document[champ] = valeurs[index];
+      });
+
+      const promise = db.add(document);
+      promises.push(promise);
+
+      totalLignes++;
+    }
+  });
+
+  rl.on('close', () => {
+    console.log('Importation en cours...');
+
+    Promise.all(promises)
+      .then(() => {
+        lignesImportees = totalLignes;
+        console.log('Importation terminée.');
+        console.log('Lignes importées :', lignesImportees);
+        menu();
+      })
+      .catch((erreur) => {
+        console.error('Erreur lors de l\'importation des documents :', erreur);
+      });
+  });
+}
+ 
+
+  function createData(object) {
+
+    db.add(objet)
+      .then((docRef) => {
+        console.log('Document ajouté avec ID :', docRef.id);
+      })
+      .catch((error) => {
+        console.error('Erreur lors de l\'ajout du document :', error);
+      });
+  }
   
   
 
@@ -97,14 +119,34 @@ function deleteData(id) {
     });
 }
 
+function askObj() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question('Entrez le nom du champ (ou tapez "fin" pour arrêter) : ', (champ) => {
+    if (champ.toLowerCase() === 'fin') {
+      rl.close();
+      createData(objet);
+    } else {
+      rl.question('Entrez la valeur : ', (valeur) => {
+        objet[champ] = valeur;
+        askObj();
+      });
+    }
+  });
+}
+
 
 function menu() {
   console.log("\nMenu principal :");
   console.log("1. Importer les données du fichier CSV");
-  console.log("2. Lire les données");
-  console.log("3. Mettre à jour une donnée");
-  console.log("4. Supprimer une donnée");
-  console.log("5. Quitter");
+  console.log("2. Créer un nouvel objet");
+  console.log("3. Lire les données");
+  console.log("4. Mettre à jour une donnée");
+  console.log("5. Supprimer une donnée");
+  console.log("6. Quitter");
 
   const rl = readline.createInterface({
     input: process.stdin,
@@ -123,14 +165,20 @@ function menu() {
         );
         break;
       case "2":
-        readData();
+        Object.keys(objet).forEach(key => {
+          delete object[key];
+        })
+        askObj();
         break;
       case "3":
+        readData();
+        break;
+      case "4":
         rl.question(
           "Veuillez saisir l'ID du document à mettre à jour : ",
           (id) => {
             rl.question(
-              "Veuillez saisir les champs à mettre à jour (au format champ:valeur) : ",
+              "Veuillez saisir les champs à mettre à jour (au format champ:valeur,) : ",
               (champs) => {
                 const champsMaj = {};
                 const champsArray = champs.split(",");
@@ -145,13 +193,13 @@ function menu() {
           }
         );
         break;
-      case "4":
+      case "5":
         rl.question("Veuillez saisir l'ID du document à supprimer : ", (id) => {
           deleteData(id);
           rl.close();
         });
         break;
-      case "5":
+      case "6":
         rl.close();
         break;
       default:
@@ -162,5 +210,4 @@ function menu() {
   });
 }
 
-// Démarrage du programme
 menu();
